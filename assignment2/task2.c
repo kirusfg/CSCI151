82c2cd79337f606ca3128260fa41c298b0fe1263
kirusfg <kirusgg@gmail.com>
2019-09-20 22:27:22 +0600

    Solving Assignment 2: edited task2

--- assignment2/task2.c
+++ assignment2/task2.c
@@ -12,38 +12,38 @@ int main(void) {
 	do {
 		ch = getc(infile);
 
-		if ((ch == '.') || (ch == '!') || (ch == '?')) {
+		if ((ch == '.') || (ch == '!') || (ch == '?') || (ch == '\n')) {	//Checking whether the sentence has ended
 			sentenceEnd = 1;
 
 			printf("%c", ch);
 			putc(ch, outfile);
-		} else if (sentenceEnd && ((ch >= 97) && (ch <= 122))) {
+		} else if (sentenceEnd && ((ch >= 97) && (ch <= 122))) {	//Checking whether the starting letter of a sentence is lowercase
 			pos = ch - 97;
 
 			printf("%c", alphabet[(26 + pos - key) % 26] - 32);
 			putc(alphabet[(26 + pos - key) % 26] - 32, outfile);
 
 			sentenceEnd = 0;
-		} else if (sentenceEnd && ((ch >= 65) && (ch <= 90))) {
+		} else if (sentenceEnd && ((ch >= 65) && (ch <= 90))) {		//Checking whether the starting letter of a sentence is uppercase
 			pos = ch - 65;
 
 			printf("%c", alphabet[(26 + pos - key) % 26] - 32);
 			putc(alphabet[(26 + pos - key) % 26] - 32, outfile);
 
 			sentenceEnd = 0;
-		} else if (!sentenceEnd && ((ch >= 97) && (ch <= 122))) {
+		} else if (!sentenceEnd && ((ch >= 97) && (ch <= 122))) {	//Checking whether the sentence letter is uppercase
 			pos = ch - 97;
 
 			printf("%c", alphabet[(26 + pos - key) % 26]);
 			putc(alphabet[(26 + pos - key) % 26], outfile);
-		} else if (!sentenceEnd && ((ch >= 65) && (ch <= 90))) {
+		} else if (!sentenceEnd && ((ch >= 65) && (ch <= 90))) {	//Checking whether the sentence letter is lowercase
 			pos = ch - 65;
 
 			printf("%c", alphabet[(26 + pos - key) % 26] - 32);
 			putc(alphabet[(26 + pos - key) % 26] - 32, outfile);
-		} else {
+		} else {													//Printing other characters
 			printf("%c", ch);
-			
+
 			if (ch != EOF) {
 				putc(ch, outfile);
 			}
