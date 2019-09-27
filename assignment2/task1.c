b9888f442140d4a0e87152c067b4e2f3dce56d10
kirusfg <kirusgg@gmail.com>
2019-09-27 15:33:16 +0600

    Solving Assignment 2: task1.c

--- assignment2/task1.c
+++ assignment2/task1.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
 	int n, i, j, k;
 
-	printf("Enter the dimensions of square matrices: ");
-	scanf("%d", &n);
+	do {
+		printf("Enter the dimensions of square matrices (positive non-zero number): ");
+		scanf("%d", &n);
+	} while (n <= 0);
 
 	int a[n][n], b[n][n], c[n][n];
 
-	//Asking user to enter each value of a[n][n] and b[n][n]s arrays
+	//Asking user to enter each value of a[n][n] and b[n][n] arrays
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < n; j++) {
 			printf("a[%d][%d] = ", i, j);
@@ -37,7 +40,7 @@ int main(void) {
 			for (k = 0; k < n; k++) {
 				c[i][j] += a[i][k] * b[k][j];
 			}
-			printf("%5d", c[i][j]);
+			printf("%-3d ", c[i][j]);
 		}
 		printf("\n");
 	}
