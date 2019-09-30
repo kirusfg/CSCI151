47d5ec226186ee72ada345f10720741d1616bf48
kirusfg <kirusgg@gmail.com>
2019-09-30 22:30:43 +0600

    Solved lesson16.4

---
+++ lesson16/lesson16.4.c
@@ -0,0 +1,33 @@
+/*
+Write a function, sumOfDigits(int n), that returns the sum 
+of digits of the non-negative number n in its decimal representation. 
+For n=129, for instance, the function returns the value 12, obtained as 1+2+9.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int sumOfDigits(int n) {
+	if (n <= 0) {
+		return 0;
+	}
+
+	int i, digits = 1, sum = 0;
+
+	for (i = 10; i <= n; i *= 10) {
+		digits++;
+	}
+
+	for (i = 1; i <= digits; i++) {
+		sum += n % 10;
+		n /= 10;
+	}
+
+	return sum;
+}
+
+int main(void) {
+	printf("%d", sumOfDigits(187));
+
+	return 0;
+}
