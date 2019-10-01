164f5ac8161533e1fc049f9582df25e09aadd711
kirusfg <kirusgg@gmail.com>
2019-10-01 20:21:52 +0600

    Solved lesson17.1

---
+++ lesson17/lesson17.1.c
@@ -0,0 +1,26 @@
+/*
+Write a function that takes an array of doubles and its 
+size as arguments, and returns the index of the smallest element.
+ */
+
+#include <stdio.h>
+
+int smallest(int size, double array[]) {
+	int i, index = 0;
+	double min = array[0];
+
+	for (i = 0; i < size; i++) {
+		min = (array[i] < min) ? array[i] : min;
+		index = i;
+	}
+
+	return index;
+}
+
+int main(void) {
+	double array[] = {10, 1023, 123, 1, -12, 5};
+
+	printf("%d", smallest(6, array));
+
+	return 0;
+}
