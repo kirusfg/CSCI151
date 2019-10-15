4150d8854e78761df672859fe216ba82dba6fc11
kirusfg <kirusgg@gmail.com>
2019-10-15 16:39:52 +0600

    Solving Assignment 3: solved task1

--- assignment3/task1.c
+++ assignment3/task1.c
@@ -6,19 +6,26 @@ typedef struct {
 } station;
 
 int readStations(char fileName[20], station line[]) {
-	FILE *inputFile = fopen(fileName, "r");
-	char ch[30];
+	char ch;
 	int i = 0;
 
+	//Opening the file, checking whether it has been opened successfully
+	FILE *inputFile = fopen(fileName, "r");
 	if (inputFile == NULL) {
 		printf("Error opening input file %s\n", fileName);
 	}
 
-	do {
+	//Returning zero if the file is empty
+	if ((ch = getc(inputFile)) == EOF) {
+		return 0;
+	}
+
+	//Scanning the file
+	while (!feof(inputFile)) {
 		fscanf(inputFile, "%lf", &line[i].km);
 		fscanf(inputFile, " %s", line[i].name);
 		i++;
-	} while (!feof(inputFile));
+	}
 
 	return i;
 }
@@ -29,6 +36,7 @@ int main(void) {
 
 	numberOfStationsScanned = readStations("london-cambridge.txt", railwayLine);
 
+	//Testing the implementation
 	while (i != numberOfStationsScanned) {
 		printf("%.1f %s\n", railwayLine[i].km, railwayLine[i].name);
 		i++;
