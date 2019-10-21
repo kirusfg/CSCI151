c6638c5ae00e975da5b0681561f825b0f2401717
kirusfg <kirusgg@gmail.com>
2019-10-21 19:37:55 +0600

    Solved lesson18.1

--- lesson18/lesson18.1.c
+++ lesson18/lesson18.1.c
@@ -9,15 +9,15 @@ double power(double num, int pow) {
 		return 1;
 	} else if (pow > 0) {
 		return num * power(num, pow - 1);
-	} else {
-		return num / power(num, pow + 1);
+	} else if (pow < 0) {
+		return 1 / (num * power(num, -pow - 1));
 	}
 }
 
 int main(void) {
-	printf("%d\n", power(2.0, 2));
-	printf("%d\n", power(2.0, -2));
-	printf("%d\n", power(2.0, 0));
+	printf("%g\n", power(2.0, 2));
+	printf("%g\n", power(2.0, -3));
+	printf("%g\n", power(2.0, 0));
 
 	return 0;
 }
